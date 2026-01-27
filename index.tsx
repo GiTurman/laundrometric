5c8644b9857d95bb9f0a51b2c9db27d62cbd04d6
GiTurman <giorgi.turman@gmail.com>
2026-01-28 03:49:31 +0400

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './index.css'; // <--- აი ეს ხაზი აკლია, რომ სტილები ჩაირთოს!
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
