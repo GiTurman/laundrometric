b868cb1effa45ac1e80639ab26a1fa2b48703f04
GiTurman <giorgi.turman@gmail.com>
2026-01-28 09:54:47 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  // 1. ვზრდით ლიმიტს, რომ გაფრთხილება გაქრეს
   build: {
+    chunkSizeWarningLimit: 2000, 
     rollupOptions: {
-      input: './index.html'
+      input: './index.html',
     }
-  }
+  },
+  // 2. ვამატებთ CSS-ის მხარდაჭერას
+  css: {
+    postcss: './postcss.config.js'
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './'),
+    },
+  },
 });
